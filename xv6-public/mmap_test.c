a61ed7e26d22cf152302f305b0a152e39c13d763
masonrware <86129546+masonrware@users.noreply.github.com>
2023-11-04 16:15:04 -0500

    updated user test program to test 4

--- xv6-public/mmap_test.c
+++ xv6-public/mmap_test.c
@@ -3,11 +3,16 @@
 #include "stat.h"
 #include "mmap.h"
 
+#include "types.h"
+#include "user.h"
+#include "stat.h"
+#include "mmap.h"
+
 int main() {
-    uint addr = 0x60020000;
+    uint addr = 0;
     int len = 4000;
     int prot = PROT_READ | PROT_WRITE;
-    int flags = MAP_ANON | MAP_FIXED | MAP_SHARED;
+    int flags = MAP_ANON | MAP_SHARED;
     int fd = -1;
 
     /* mmap anon memory */
@@ -15,7 +20,14 @@ int main() {
     if (mem == (void *)-1) {
         goto failed;
     }
-    if (mem != (void *)addr) {
+
+    /* Modify something */
+    char *memchar = (char*) mem;
+    memchar[0] = 'a'; memchar[1] = 'a';
+
+    /* Clean and return */
+    int ret = munmap(mem, len);
+    if (ret < 0) {
         goto failed;
     }
 
@@ -27,3 +39,28 @@ failed:
     printf(1, "MMAP\t FAILED\n");
     exit();
 }
+
+// int main() {
+//     uint addr = 0x60020000;
+//     int len = 4000;
+//     int prot = PROT_READ | PROT_WRITE;
+//     int flags = MAP_ANON | MAP_FIXED | MAP_SHARED;
+//     int fd = -1;
+
+//     /* mmap anon memory */
+//     void *mem = mmap((void *)addr, len, prot, flags, fd, 0);
+//     if (mem == (void *)-1) {
+//         goto failed;
+//     }
+//     if (mem != (void *)addr) {
+//         goto failed;
+//     }
+
+// // success:
+//     printf(1, "MMAP\t SUCCESS\n");
+//     exit();
+
+// failed:
+//     printf(1, "MMAP\t FAILED\n");
+//     exit();
+// }

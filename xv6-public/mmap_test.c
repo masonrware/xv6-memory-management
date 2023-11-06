cb81821dc5200e97cf8ac2c35ea36ac7723f7080
MASON WARE <mware@vm-instunix-01.cs.wisc.edu>
2023-11-05 22:59:54 -0600

    working with user program test

--- xv6-public/mmap_test.c
+++ xv6-public/mmap_test.c
@@ -16,52 +16,89 @@ int my_strcmp(const char *a, const char *b, int n) {
 int main() {
     char *filename = "test_file.txt";
     int len = 100;
-    char buff[len];
     int prot = PROT_READ | PROT_WRITE;
+//    int flags = MAP_PRIVATE;
     int flags = MAP_SHARED;
+    char buff[len];
+    char new_buff[len];
 
     /* Open a file */
     int fd = open(filename, O_CREATE | O_RDWR);
     if (fd < 0) {
         printf(1, "Error opening file\n");
-    goto failed;
+        goto failed;
     }
 
     /* Write some data to the file */
     for (int i = 0; i < len; i++) {
         buff[i] = (char)(i % 256);
     }
+
     if (write(fd, buff, len) != len) {
         printf(1, "Error: Write to file FAILED\n");
-    goto failed;
+        goto failed;
     }
-    close(fd);
 
-    fd = open(filename, O_CREATE | O_RDWR);
-
-    /* Memory map the file */
+    /* mmap the file */
     void *mem = mmap(0, len, prot, flags, fd, 0);
     if (mem == (void *)-1) {
         printf(1, "mmap FAILED\n");
         goto failed;
     }
+    
+    /* Moify the mmapped memory */
+    char *charmem = (char*)mem;
+    for(int i = 0; i < len; i++) {
+	    charmem[i] = 'a';
+    	    new_buff[i] = charmem[i];
+	}
+     
+    printf(1, "%s\n", new_buff);
+ 
+    /* Fork */
+    int pid = fork();
+    if (pid == 0) {
+        /* Verify the child can read the same data */
+        printf(1, "%s\n", (char *)mem);
+        char *mem_buff = (char *)mem;
+        printf(1, "%s\n", mem_buff);
+        if (my_strcmp(mem_buff, new_buff, len) != 0) {
+            printf(1, "Data mismatch in child\n");
+            goto failed;
+        }
 
-    /* Verify the data in mmaped memory is the same as what was written */
-    char *mem_buff = (char *)mem;
-    printf(1, "from mmap: %s\ncopy of mmap: %s\n");
-    if (my_strcmp(mem_buff, buff, len) != 0) {
-        printf(1, "Couldn't read the same data back!\n");
-        goto failed;
+	/* Modify data in child - shouldn't affect parent */
+	for(int i = 0; i < len; i++)
+		mem_buff[i] = 'b';
+	
+    printf(1, "74\n");
+
+	/* Child success - exit */
+	exit();
+    } else {
+        wait();
+    
+    printf(1, "81\n");
+
+	/* Verify the child modifications are not seen by the parent */
+	char *mem_buff = (char*)mem;
+	if(my_strcmp(mem_buff, new_buff, len) != 0) {
+		printf(1, "Parent data corrupted by child\n");
+		goto failed;
+	}
+    printf(1, "89\n");
+        /* Clean and return */
+        int ret = munmap(mem, len);
+        if (ret < 0) {
+            printf(1, "munmap failed\n");
+            goto failed;
+        }
+
+        close(fd);
     }
 
-    /* Clean and return */
-    int ret = munmap(mem, len);
-    if (ret < 0) {
-        printf(1, "munmap FAILED\n");
-    goto failed;
-    }
 
-    close(fd);
+
 
 // success:
     printf(1, "MMAP\t SUCCESS\n");
@@ -71,4 +108,3 @@ failed:
     printf(1, "MMAP\t FAILED\n");
     exit();
 }
-
